7ec4d129d7266e4cf15d59757cd905edd678a4dd
vasu <pentapativasu3333@gmail.com>
2023-07-04 11:53:52 +0530

    first

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -4,6 +4,7 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
+    @Autowired
 
+HotelManagementSerive hotelManagementSerive;
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -28,9 +30,9 @@ public class HotelManagementController {
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
+     String s=hotelManagementSerive.addHotel(hotel);
 
-
-        return null;
+        return s;
     }
 
     @PostMapping("/add-user")
@@ -38,8 +40,8 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
-
-       return null;
+        Integer n=hotelManagementSerive.addUser(user);
+       return n;
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -48,8 +50,8 @@ public class HotelManagementController {
         //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
-
-        return null;
+       String s=hotelManagementSerive.getHotelWithMostFacilities();
+        return s;
     }
 
     @PostMapping("/book-a-room")
@@ -61,15 +63,16 @@ public class HotelManagementController {
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
-        
-        return 0;
+        int n=hotelManagementSerive.bookARoom(booking);
+        return n;
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
-    public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
+    public String getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
-        //In this function return the bookings done by a person 
-        return 0;
+        //In this function return the bookings done by a person
+        String s=hotelManagementSerive.getBookings(aadharCard);
+        return s;
     }
 
     @PutMapping("/update-facilities")
@@ -79,7 +82,8 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return null;
+        Hotel h=hotelManagementSerive.updateFacilities(newFacilities,hotelName);
+        return h;
     }
 
 }
