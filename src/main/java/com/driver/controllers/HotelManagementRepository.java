40305098df1226082d3f29d787558510fd9876cb
vasu <pentapativasu3333@gmail.com>
2023-08-04 11:24:01 +0530

    addingseventeen

--- src/main/java/com/driver/controllers/HotelManagementRepository.java
+++ src/main/java/com/driver/controllers/HotelManagementRepository.java
@@ -14,46 +14,47 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class HotelManagementRepository {
-      static Map<String,Hotel>hotelDb=new HashMap<>();
-      Map<Integer, User> userDb=new HashMap<>();
+    static Map<String, Hotel> hotelDb = new HashMap<>();
+    Map<Integer, User> userDb = new HashMap<>();
 
-      Map<String,Booking> bookingDb=new HashMap<>();
+    Map<String, Booking> bookingDb = new HashMap<>();
 
-      Map<Integer,Integer>countOfBookings=new HashMap<>();
+    Map<Integer, Integer> countOfBookings = new HashMap<>();
 //    final AtomicInteger aadharCardCounter = new AtomicInteger(1);
 
-    public  String addHotel(Hotel hotel) {
-    if(hotel.getHotelName() == null){
-        return "FAILURE";
-    }
-     else if(hotelDb.containsKey(hotel.getHotelName())){
-         return "FAILURE";
-    }
-     else{
-        hotelDb.put(hotel.getHotelName(),hotel);
-        return "SUCCESS";
-    }
+    public String addHotel(Hotel hotel) {
+        if (hotel.getHotelName() == null) {
+            return "FAILURE";
+        } else if (hotelDb.containsKey(hotel.getHotelName())) {
+            return "FAILURE";
+        } else {
+            hotelDb.put(hotel.getHotelName(), hotel);
+            return "SUCCESS";
+        }
 
     }
 
     public int addUser(User user) {
 //        int adhaarCardNo=aadharCardCounter.getAndIncrement();
 //        user.setaadharCardNo(adhaarCardNo);
-        userDb.put(user.getaadharCardNo(),user);
+        userDb.put(user.getaadharCardNo(), user);
         return user.getaadharCardNo();
     }
 
-    public  String getHotelWithMostFacilities() {
-        int facilities=0;
-        String HotelName="";
-        for(Hotel hotel: hotelDb.values()){
-            if(hotel.getFacilities()!=null &&   hotel.getFacilities().size() > facilities){
-                facilities=hotel.getFacilities().size();
-//                HotelName=hotel.getHotelName();
-            }
-            else if(hotel.getFacilities()!=null && hotel.getFacilities().size()==facilities){
-                if(hotel.getHotelName().compareTo(HotelName)<0){
-                    HotelName=hotel.getHotelName();
+    public String getHotelWithMostFacilities() {
+        int facilities = 0;
+        String HotelName = "";
+
+        for (Hotel hotel : hotelDb.values()) {
+
+            if (hotel.getFacilities() == null) continue;
+
+            if (hotel.getFacilities().size() > facilities) {
+                facilities = hotel.getFacilities().size();
+                HotelName=hotel.getHotelName();
+            } else if (hotel.getFacilities().size() == facilities) {
+                if (hotel.getHotelName().compareTo(HotelName) < 0) {
+                    HotelName = hotel.getHotelName();
                 }
             }
         }
@@ -61,27 +62,26 @@ public class HotelManagementRepository {
     }
 
     public int bookARoom(Booking booking) {
-        String key= UUID.randomUUID().toString();
+        String key = UUID.randomUUID().toString();
         booking.setBookingId(key);
-        String hotelName= booking.getHotelName();
-        Hotel hotel=hotelDb.get(hotelName);
-        int avialbleRooms=hotel.getAvailableRooms();
-        if(avialbleRooms < booking.getNoOfRooms()){
+        String hotelName = booking.getHotelName();
+        Hotel hotel = hotelDb.get(hotelName);
+        int avialbleRooms = hotel.getAvailableRooms();
+        if (avialbleRooms < booking.getNoOfRooms()) {
             return -1;
         }
-        int amountToBePaid= hotel.getPricePerNight()*booking.getNoOfRooms();
+        int amountToBePaid = hotel.getPricePerNight() * booking.getNoOfRooms();
         booking.setAmountToBePaid(amountToBePaid);
 
-        hotel.setAvailableRooms(hotel.getAvailableRooms()-booking.getNoOfRooms());
-        bookingDb.put(key,booking);
-        hotelDb.put(hotelName,hotel);
-        int aadhaarCard=booking.getBookingAadharCard();
-        if(countOfBookings.containsKey(aadhaarCard)){
-            int totalbookings=countOfBookings.get(aadhaarCard);
-            countOfBookings.put(aadhaarCard,totalbookings+1);
-        }
-        else {
-            countOfBookings.put(aadhaarCard,1);
+        hotel.setAvailableRooms(hotel.getAvailableRooms() - booking.getNoOfRooms());
+        bookingDb.put(key, booking);
+        hotelDb.put(hotelName, hotel);
+        int aadhaarCard = booking.getBookingAadharCard();
+        if (countOfBookings.containsKey(aadhaarCard)) {
+            int totalbookings = countOfBookings.get(aadhaarCard);
+            countOfBookings.put(aadhaarCard, totalbookings + 1);
+        } else {
+            countOfBookings.put(aadhaarCard, 1);
         }
         return amountToBePaid;
     }
@@ -91,18 +91,17 @@ public class HotelManagementRepository {
     }
 
     public Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
-        List<Facility>oldfacilities=hotelDb.get(hotelName).getFacilities();
-         for(Facility facilities:newFacilities){
-             if(oldfacilities.contains(facilities)){
-                 continue;
-             }
-             else{
-                 oldfacilities.add(facilities);
-             }
-         }
-         Hotel hotel=hotelDb.get(hotelName);
-         hotel.setFacilities(oldfacilities);
-         hotelDb.put(hotelName,hotel);
-         return hotel;
+        List<Facility> oldfacilities = hotelDb.get(hotelName).getFacilities();
+        for (Facility facilities : newFacilities) {
+            if (oldfacilities.contains(facilities)) {
+                continue;
+            } else {
+                oldfacilities.add(facilities);
+            }
+        }
+        Hotel hotel = hotelDb.get(hotelName);
+        hotel.setFacilities(oldfacilities);
+        hotelDb.put(hotelName, hotel);
+        return hotel;
     }
 }
